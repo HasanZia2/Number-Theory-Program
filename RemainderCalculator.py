d4a222b6d6824f54c49b24c7e331c2d512aec37b
Hasan Farhan Zia <h.zia2396@gmail.com>
2026-05-28 23:11:03 -0600

    Completed exponentiation part of RemainderCalculator.

--- RemainderCalculator.py
+++ RemainderCalculator.py
@@ -18,13 +18,14 @@ def OddPrime(prime):
                 break
     return odd_prime
 
-def FermatsLittleTheorem():
+def FermatsLittleTheorem(prime, base, exponent):
     # This function applies Fermat's Little Theorem to solve
     # remainder problems involving exponentiation.
 
-    pass
+    reducedExponent = exponent%(prime - 1)
+    return base**reducedExponent%prime
 
-def WilsonsTheorem():
+def WilsonsTheorem(prime, factorial):
     # This function applies Wilson's Theorem to solve
     # remainder problems involving factorials.
 
@@ -38,11 +39,27 @@ def RemainderCalculator():
     print("This program finds the remainder when dividing large natural numbers (involving exponentiation or factorials) by prime numbers. ")
     print(" ")
     prime = int(input("Enter the prime you would like to divide by. "))
+    print(" ")
     while not OddPrime(prime) and prime != 2:
         prime = int(input("The number you entered is not prime. Please try again. "))
-    type = input("Does the large number you want to divide involve exponentiation or factorials? (Enter \"exponentiation\" or \"factorial\".) ").lower
-    while type != "exponentiation" and type != "factorial":
-        type = input("You did not correctly select exponentiation or factorials. Please try again. (Enter \"exponentiation\" or \"factorial\".) ").lower
+    type = input("Does the large number you want to divide involve exponentiation or factorials? (Enter \"E\" for the former or \"F\" for the latter.) ").upper()
+    while type != "E" and type != "F":
+        type = input("You did not correctly select exponentiation or factorials. Please try again. (Enter \"E\" for the former or \"F\" for the latter.) ").upper()
+    print(" ")
+    if type == "E":
+        base = int(input("Enter the base (a natural number) of the exponential expression. "))
+        while base <= 0:
+            base = int(input("The number you entered is either negative or zero. Please try again. "))
+        exponent = int(input("Enter the exponent (a natural number) of the exponential expression. "))
+        while exponent <= 0:
+            exponent = int(input("The number you entered is either negative or zero. Please try again. "))
+        remainder = FermatsLittleTheorem(prime, base, exponent)
+        print("The remainder on dividing " + str(base) + "^" + str(exponent) + " by " + str(prime) + " is " + str(remainder) + ". ")
+    else:
+        factorial = int(input("Enter the number (a natural number) whose factorial is being taken. "))
+        while factorial <= 0:
+            factorial = int(input("The number you entered is either negative or zero. Please try again. "))
+        WilsonsTheorem(prime, factorial)
 
 def main():
     RemainderCalculator()
